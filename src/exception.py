1454b97d77ef13da885d0d70f2cff3762220d282
Amit <ranjamit1@gmail.com>
2025-08-16 20:59:01 +0530

    Add Exception

---
+++ src/exception.py
@@ -0,0 +1,30 @@
+import sys
+from src.logger import logging
+from typing import Tuple
+def error_message_detail(error: Exception, error_detail:Tuple=sys.exc_info()):
+    """
+    Constructs a detailed error message from an exception object.
+    """
+    _, _, exc_tb = error_detail
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "error occured in pyhton script name [{0}] line number [{1}] error [{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message:Exception,error_detail:Tuple=sys.exc_info()):
+        #calling base class constructor 
+        super().__init__(error_message)
+
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message 
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide  By zero error")
+        raise CustomException(e,sys.exc_info())
+    
